8b6bec56334e11acea32fffaf55d93de4e48e9b0
dorF <nemoz.burkhalter@gmail.com>
2022-07-06 12:12:31 +0200

    Convex shapes can now be created with right click

--- Implementation/src/World.cpp
+++ Implementation/src/World.cpp
@@ -8,6 +8,7 @@
 #include <ranges>
 
 #include "CircleShape.h"
+#include "ConvexShape.h"
 
 void World::AddEntity(std::unique_ptr<Entity> ent)
 {
@@ -28,7 +29,6 @@ void World::Start()
 
     //Move camera to be in center of the screen
     cameraPos_ = SFMLUtils::sfVectorToVector2(static_cast<sf::Vector2f>(window_->getSize())) * -0.5;
-    std::cout << cameraPos_.ToString();
 
 	while (window_->isOpen())
 	{
@@ -97,18 +97,36 @@ void World::UpdatePhysics(const double deltaTime)
 
 void World::ClickEvent(const sf::Event event)
 {
-    std::cout << std::to_string(sf::Mouse::getPosition(*window_).x) + " " + std::to_string(sf::Mouse::getPosition(*window_).y) + " "
-	+ SFMLUtils::ScreenToWorldPos(static_cast<sf::Vector2f>(sf::Mouse::getPosition(*window_)), cameraPos_).ToString() + " " + cameraPos_.ToString();
-
     std::random_device rd;
     std::default_random_engine eng(rd());
-    const std::uniform_real_distribution<double> distr(-5.0, 5.0);
 
 	if (event.mouseButton.button == sf::Mouse::Left)
 	{
-        double rad = abs(distr(eng)) + 1;
+        const std::uniform_real_distribution<double> radiusdistr(0.0, 3.0);
+
+        double rad = abs(radiusdistr(eng)) + 1;
         auto rb = std::make_unique<Rigibody>(std::make_unique<CircleShape>(rad),
             SFMLUtils::ScreenToWorldPos((static_cast<sf::Vector2f>(sf::Mouse::getPosition(*window_))), cameraPos_), Vector2D(), rad);
         AddEntity(std::make_unique<Entity>(std::move(rb)));
 	}
+    else if (event.mouseButton.button == sf::Mouse::Right)
+    {
+        const std::uniform_int_distribution<int> edgesNumDistr(0, 5);
+        const std::uniform_real_distribution<double> edgeDistr(-3.0, 3.0);
+
+        int edges = abs(static_cast<int>(edgesNumDistr(eng)));
+        if (edges < 3)
+            edges = 3;
+        std::vector<Vector2D> points(edges);
+        for (int e = 0; e < edges; e++)
+        {
+            points.emplace_back(Vector2D(edgeDistr(eng), edgeDistr(eng)));
+        }
+
+        ConvexShape shape(points);
+        auto rb = std::make_unique<Rigibody>(std::make_unique<ConvexShape>(shape),
+            SFMLUtils::ScreenToWorldPos((static_cast<sf::Vector2f>(sf::Mouse::getPosition(*window_))), cameraPos_),
+            Vector2D(), edges, false, true);
+        AddEntity(std::make_unique<Entity>(std::move(rb)));
+    }
 }
--- Implementation/src/main.cpp
+++ Implementation/src/main.cpp
@@ -9,8 +9,7 @@ int main()
 {
     World world(std::make_unique<sf::RenderWindow>(sf::VideoMode(1920, 1080), "2DPhysics Demoz Nemoz"), 1 / 60.0);
 
-#pragma region Create Bodies
-
+#pragma region Create Floor
     world.AddEntity(std::make_unique<Entity>(std::make_unique<Rigibody>(std::make_unique<ConvexShape>(std::vector<Vector2D> 
     {Vector2D(-5.0, -1.0), Vector2D(5.0, -1.0), Vector2D(-5.0, 1.0), Vector2D(0.0, 10.0), Vector2D(5.0, 1.0)}),
         Vector2D(-15.0, -5.0), Vector2D(), 1.0, true)));
@@ -22,42 +21,7 @@ int main()
     world.AddEntity(std::make_unique<Entity>(std::make_unique<Rigibody>(std::make_unique<ConvexShape>(std::vector<Vector2D>
     {Vector2D(-20.0, -2.0), Vector2D(20.0, -2.0), Vector2D(-20.0, 2.0), Vector2D(20.0, 2.0)}),
         Vector2D(0.0, -10.5), Vector2D(), 1.0, true)));
-//    srand(time(nullptr));
-//    for (int i = 0; i < 20 ; i++)
-//    {
-//	   if (rand() % 2 == 0)
-//	   {
-//            int edges = rand() % 10;
-//            if (edges < 3)
-//                edges = 3;
-//            std::vector<Vector2D> points(edges);
-//            for (int e = 0; e < edges; e++)
-//            {
-//                points.emplace_back(Vector2D(distr(eng), distr(eng)));
-//            }
-//
-//            ConvexShape shape(points);
-//            auto rb = std::make_unique<Rigibody>(std::make_unique<ConvexShape>(shape),
-//                Vector2D(rand() % 30 - 15, rand() % 30 - 15), Vector2D(/*rand() % 10 - 5, rand() % 10 - 5*/), edges, false, false);
-//            world.AddEntity(std::make_unique<Entity>(std::move(rb)));
-//	   }
-//        else
-//        {
-//        double rad = rand() % 5 + 0.1;
-//            auto rb = std::make_unique<Rigibody>(std::make_unique<CircleShape>(rad),
-//                Vector2D(rand() % 30 - 15, rand() % 30 - 15), Vector2D(rand() % 10 - 5, rand() % 10 - 5), rad, false, false);
-//            world.AddEntity(std::make_unique<Entity>(std::move(rb)));
-//        }
-//
-//        std::cout << "made shape" + std::to_string(i) << std::endl;
-//    }
-//
-//    
 #pragma endregion
-
-    //Testing polygonal shapes
-    
-    //std::cout << shape.GetFurthestPoint(Vector2D(-1.0, 0.0)).ToString() << std::endl;
 
     world.Start();
 
